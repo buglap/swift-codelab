4362db3afaab9ef9a14edfaed385d0df50f0d514
jenny.escobar <jenny.escobar@pragma.com.co>
2022-04-05 16:54:08 -0500

    Initial Commit

---
+++ swiftui-codelab/swiftui_codelabApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_codelabApp.swift
+//  swiftui-codelab
+//
+//  Created by Jenny escobar on 5/04/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_codelabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
